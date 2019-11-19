ce3dd221ace7ff171f46dcf994ac777afc9c4651
Thierry P.G. Decker <mail@thierry-decker.com>
2019-11-19 19:36:39 +0100

    Create Login signals

--- api/applications/login/apps.py
+++ api/applications/login/apps.py
@@ -14,3 +14,11 @@ class LoginConfig(AppConfig):
     # Application description
     #
     verbose_name = 'Credentials administration'
+
+    def ready(self):
+        from api.applications.login import signals
+        #
+        # Call this dummy function in signals to avoid suppression of the above import statement
+        # as it can be considered as unused by PyCharm "optimize import"
+        #
+        signals.loaded()
