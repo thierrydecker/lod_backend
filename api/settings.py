208df9fb6ef86c6d35dfaa19cf4fa856c9635cef
Thierry P.G. Decker <mail@thierry-decker.com>
2019-11-19 06:43:32 +0100

    Create LOD middleware

--- api/settings.py
+++ api/settings.py
@@ -76,6 +76,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #
+    # LOD middleware
+    #
+    'api.middlewares.lod.apps.LodMiddleware',
 ]
 
 #
