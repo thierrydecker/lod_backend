4edacffd01e50059c4612feb42f829b925c8defd
Thierry P.G. Decker <mail@thierry-decker.com>
2019-11-24 08:58:26 +0100

    Remove inlines

--- api/applications/membership/admin.py
+++ api/applications/membership/admin.py
@@ -8,8 +8,3 @@ class MembershipAdmin(admin.ModelAdmin):
     list_display = ['related_workspace', 'related_login']
     readonly_fields = ['id']
     list_filter = ('related_login', 'related_workspace')
-
-
-class MembershipInline(admin.TabularInline):
-    model = Membership
-    extra = 0
